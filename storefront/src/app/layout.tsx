4ad18e2d22631e926f8e491a6abe3f6c82eded61
Code Head <codehead@codelegends.com>
2025-03-02 20:09:59 -0500

    initial commit

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -11,9 +11,6 @@ export default function RootLayout(props: { children: React.ReactNode }) {
     <html lang="en" data-mode="light">
       <body>
         <main className="relative">{props.children}</main>
-        <footer>
-          <h1>THIS IS A TEST FOOTER blah</h1>
-        </footer>
       </body>
     </html>
   )
